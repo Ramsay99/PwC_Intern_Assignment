b730e1963eb1c8448c719f3cf2ee59c8989c9016
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2023-01-03 11:30:32 +0300

    test api GET
    successfully called a GET method from free API website (geocode.maps.co).
    I was not expecting it to work. However, I managed to get it working. Next, I will be looking for Map Plug-In.

--- map/lib/main.dart
+++ map/lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'screens/pwc_map.dart';
 
 void main(List<String> args) {
+  _geolocationTest().then((value) => debugPrint(value));
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: PwcMap(),
   ));
+}
+
+Future<String> _geolocationTest({String address = 'jordan, amman'}) async {
+  String geolocationJSONText = '';
+  const String authority = 'geocode.maps.co';
+  const String unencodedPath = '/search';
+  Uri urlSearch = Uri.https(authority, unencodedPath, {
+    'q': {address}
+  });
+
+  var response = await http.get(urlSearch);
+  geolocationJSONText += ('\nRequest: ${response.request}');
+  if (response.statusCode == 200) {
+    geolocationJSONText += ('\nBody: ${response.body}');
+  } else {
+    geolocationJSONText += ('\nStatus Failur Code: ${response.statusCode}');
+  }
+
+  return geolocationJSONText;
 }
