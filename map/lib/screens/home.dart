3d68f98c9fb21c985ea659396a2dc6fbaf6b6316
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2023-01-03 14:17:06 +0300

    minor clean for home.dart
    separated children into two methods

--- map/lib/screens/home.dart
+++ map/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:map/map.dart';
+import 'package:pwcmap/util/LatLng.dart';
 import '../widgets/scaffold_map.dart';
 
 class Pwcmap extends StatefulWidget {
@@ -11,85 +12,102 @@ class Pwcmap extends StatefulWidget {
 
 class _PwcmapState extends State<Pwcmap> {
   @override
+  void initState() {
+    final controller = MapController(
+      location: const LatLng(0, 0),
+      zoom: 2,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold_pwcmap(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height - 120,
-            child: const Image(
-              image: NetworkImage(
-                  'https://th.bing.com/th/id/R.682aa81619f08a3e70882caabc02bf1a?rik=66qZRlxw%2fUF4uQ&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f05%2fBlack-Wallpapers.jpg&ehk=LNjUriMFdpN63FITBbYU1YwLtThAvtDX1iKQOd7KfoY%3d&risl=&pid=ImgRaw&r=0'),
-              fit: BoxFit.fill,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, bottom: 10),
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Row(
-                children: [
-                  SizedBox(
-                    height: 35,
-                    width: 220,
-                    child: TextFormField(
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        //add prefix icon
-                        prefixIcon: const Icon(
-                          Icons.map,
-                          color: Colors.grey,
-                        ),
+          map(context),
+          bottomSearchBar(),
+        ],
+      ),
+    );
+  }
 
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
+  SizedBox map(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height - 120,
+      child: const Image(
+        image: NetworkImage(
+            'https://th.bing.com/th/id/R.682aa81619f08a3e70882caabc02bf1a?rik=66qZRlxw%2fUF4uQ&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f05%2fBlack-Wallpapers.jpg&ehk=LNjUriMFdpN63FITBbYU1YwLtThAvtDX1iKQOd7KfoY%3d&risl=&pid=ImgRaw&r=0'),
+        fit: BoxFit.fill,
+      ),
+    );
+  }
 
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: Colors.blue, width: 1.0),
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        fillColor: Colors.grey,
-
-                        //make hint text
-                        hintStyle: TextStyle(
-                          color: Colors.grey.shade900,
-                          fontSize: 16,
-                          fontFamily: "verdana_regular",
-                          fontWeight: FontWeight.w400,
-                        ),
-
-                        //create lable
-                        labelText: 'City Name',
-                        //lable style
-                        labelStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                          fontFamily: "verdana_regular",
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
+  Padding bottomSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, bottom: 10),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Row(
+          children: [
+            SizedBox(
+              height: 35,
+              width: 220,
+              child: TextFormField(
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: const Icon(
+                    Icons.map,
+                    color: Colors.grey,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Search'),
-                    ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ],
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'City Name',
+                  //lable style
+                  labelStyle: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Search'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
